8549b2f2e83b73051b850dd31571401237770cc8
BerliyanoAL <149349373+BerliyanoAL@users.noreply.github.com>
2023-10-30 11:23:23 +0700

    Add files via upload

---
+++ SI_REGPAGI_22166036_latihan18.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi_22166036_latihan18;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SI_REGPAGI_22166036_latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bilangan kelipatan 3.5 dari 3.5 hingga 35:");
+
+        for (double i = 2.5; i <= 45; i+= 0.5) {
+            // Cek apakah bilangan merupakan kelipatan 3.5 
+            if (i % 2.5 == 0) {
+                System.out.println(i);
+    }
+        }
+    }
+}
+    
+    
+
